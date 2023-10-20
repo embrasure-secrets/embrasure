132066efae988885c540224c092f9a6f2ef59ffd
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-10-20 15:12:03 -0700

    refactor(wrapper.js): :recycle: update wrapper.js
    update wrapper to be a module that takes arguments for Node's absolute path, the argument file's absolute path, and the env object
    
    Co-Authored-By: ivanfslee <57885956+ivanfslee@users.noreply.github.com>

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,29 +1,22 @@
 // import spawn method of node:child_process module
 import { spawn } from 'child_process';
 
-const args = process.argv;
-// get node's absolute path
-const NODE = args[0];
+function injectSecrets(NODE, FILE_NAME, env) {
+    const child = spawn(NODE, [FILE_NAME], { env });
 
-// get current directory's absolute path
-const CURRENT_DIRECTORY = process.cwd();
+    // log child process output
+    child.stdout.on('data', (data) => {
+        console.log(`Child Process Output: ${data}`);
+    });
 
-// get filename passed in as argument. assume thats it is the argument
-const FILE_NAME = `${CURRENT_DIRECTORY}/${args[3]}`;
+    // log child process errors
+    child.stderr.on('data', (data) => {
+        console.error(`Child Process Error: ${data}`);
+    });
 
-const env = { PORT: 3000 };
-const child = spawn(NODE, [FILE_NAME], { env });
+    child.on('close', (code) => {
+        console.log(`Child Process exited with code ${code}`);
+    });
+}
 
-// log child process output
-child.stdout.on('data', (data) => {
-    console.log(`Child Process Output: ${data}`);
-});
-
-// log child process errors
-child.stderr.on('data', (data) => {
-    console.error(`Child Process Error: ${data}`);
-});
-
-child.on('close', (code) => {
-    console.log(`Child Process exited with code ${code}`);
-});
+export default injectSecrets;
