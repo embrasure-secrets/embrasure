2c5174c9b805224cafd5c238711781916fbf076c
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-10-23 08:15:49 -0700

    create aws rds postgres db instance :construction:
    programmatically create an aws rds postgres db instance and export functionality with a createPostgresInstance function that takes an array of vpc security group id's as an argument
    
    Co-Authored-By: ivanfslee <57885956+ivanfslee@users.noreply.github.com>
    Co-Authored-By: Taron <88250538+taronavagyan@users.noreply.github.com>

---
+++ src/utils/aws-init/createPostgresInstance.js
@@ -0,0 +1,29 @@
+import { RDSClient, CreateDBInstanceCommand } from "@aws-sdk/client-rds";
+
+async function createPostgresInstance(VpcSecurityGroupIds) {
+  const client = new RDSClient({ region: "us-east-1" });
+
+  const params = {
+    AllocatedStorage: 20,
+    DBInstanceIdentifier: "secrets-db5",
+    DBInstanceClass: "db.t3.micro",
+    DBName: "secrets",
+    // this is hard-coded but it shouldn't be
+    VpcSecurityGroupIds,
+    Engine: "postgres",
+    MasterUsername: "postgres",
+    MasterUserPassword: "password",
+    DBSubnetGroupName: "my-db-subnet-group",
+  };
+
+  try {
+    const createDBInstanceCommand = new CreateDBInstanceCommand(params);
+
+    const data = await client.send(createDBInstanceCommand);
+    console.log("Postgres instance created:", data);
+  } catch (error) {
+    console.error("Error creating Postgres instance:", error);
+  }
+}
+
+export default createPostgresInstance;
