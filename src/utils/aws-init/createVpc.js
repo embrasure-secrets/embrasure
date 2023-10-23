1edd8eb01b8098eefa0e74b9df1ccc08e5853a0a
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-10-23 15:33:32 -0700

    update createVpc 🐛
    add ModifyVpcAttributeCommand calls to createVpc function so that EnableDnsHostnames and EnableDnsSupport properties of vpc are set to enabled for created VPC

--- src/utils/aws-init/createVpc.js
+++ src/utils/aws-init/createVpc.js
@@ -1,10 +1,15 @@
-import { EC2Client, CreateVpcCommand, CreateTagsCommand } from '@aws-sdk/client-ec2';
+import {
+    EC2Client,
+    CreateVpcCommand,
+    ModifyVpcAttributeCommand,
+    CreateTagsCommand,
+} from '@aws-sdk/client-ec2';
 
 async function createVpc() {
-    const ec2Client = new EC2Client({ region: 'us-east-1' });
+    const client = new EC2Client({ region: 'us-east-1' });
 
     const createVpcCommand = new CreateVpcCommand({ CidrBlock: '10.0.0.0/16' });
-    const response = await ec2Client.send(createVpcCommand);
+    const response = await client.send(createVpcCommand);
 
     // Code below this point is just adding a name to the newly created VPC
     const tagsParams = {
@@ -12,13 +17,27 @@ async function createVpc() {
         Tags: [
             {
                 Key: 'Name',
-                Value: 'Embrasure-VPC',
+                Value: 'Embrasure-VPC-v2',
             },
         ],
     };
 
     const createTagsCommand = new CreateTagsCommand(tagsParams);
-    await ec2Client.send(createTagsCommand);
+    await client.send(createTagsCommand);
+
+    const vpcModifyInput1 = {
+        VpcId: response.Vpc.VpcId,
+        EnableDnsHostnames: { Value: true },
+    };
+    const vpcModifyInput2 = {
+        VpcId: response.Vpc.VpcId,
+        EnableDnsSupport: { Value: true },
+    };
+    const command1 = new ModifyVpcAttributeCommand(vpcModifyInput1);
+    await client.send(command1);
+
+    const command2 = new ModifyVpcAttributeCommand(vpcModifyInput2);
+    await client.send(command2);
     return response;
 }
 
