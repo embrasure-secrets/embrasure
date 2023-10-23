4e84d435bafe620cf463fd1942f9a05c87260d48
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-10-23 14:18:06 -0700

    feat(cli): :sparkles: create basic embrasure CLI
    create basic CLI with commander.
    
    add functionality for getting all secrets
    
    add functionality for getting specified secret
    
    add functionality for adding a secret
    
    add functionality for updating a secret
    
    add functionality for deleting a secret
    
    Error handling/testing not yet created.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,67 @@
 #!/usr/bin/env node
 
-console.log('Hello World!');
+/*
+Running each of the functions below leads to a huge wall of text being logged to the console, needs fixing.
+
+The return values of the functions is not standardized. Some return the resource you're looking for, others return `undefined` and log something to the console. 
+
+Need to remove side-effects, have pure functions.
+
+Connection to DB does not exit immediately after each function is executed. There is usually a few seconds of delay.
+*/
+
+import { Command } from 'commander';
+import getAllSecrets from '../src/utils/rds/getAllSecrets.js';
+import getSecret from '../src/utils/rds/getSecret.js';
+import addSecret from '../src/utils/rds/addSecret.js';
+import updateSecret from '../src/utils/rds/updateSecret.js';
+import deleteSecret from '../src/utils/rds/deleteSecret.js';
+
+const cli = new Command();
+
+cli.version('1.0.0').description('Welcome to Embrasure Secrets Manager');
+
+cli.command('ga')
+    .alias('getAllSecrets')
+    .description('Get all secrets for your current project environment')
+    .action(async () => {
+        const secrets = await getAllSecrets();
+        console.log(secrets);
+    });
+
+cli.command('gs')
+    .alias('getSecret')
+    .description('Get secret with specified name for your current project   environment')
+    .option('-n --name <name>', 'Specify secret name')
+    .action(async ({ name }) => {
+        const secret = await getSecret(name);
+        console.log(secret);
+    });
+
+cli.command('as')
+    .alias('addSecret')
+    .description('Add secret with specified name and value')
+    .option('-n --name <name>', 'Specify secret name')
+    .option('-v --value <value>', 'Specify secret value')
+    .action(({ name, value }) => {
+        addSecret(name, value);
+    });
+
+cli.command('us')
+    .alias('updateSecret')
+    .description('Update secret with specified name')
+    .option('-n --name <name>', 'Specify secret name')
+    .option('-v --value <value>', 'Specify secret value')
+    .action(({ name, value }) => {
+        updateSecret(name, value);
+    });
+
+cli.command('ds')
+    .alias('deleteSecret')
+    .description('Delete secret with specified name')
+    .option('-n --name <name>', 'Specify secret name')
+    .action(({ name }) => {
+        deleteSecret(name);
+    });
+
+cli.parse(process.argv);
