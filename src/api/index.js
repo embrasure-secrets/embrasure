8a01a81a320533dab7bfa9643fdba1ce0581154c
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-10-26 13:38:20 -0700

    feat(api): :sparkles: create api endpoint for updating secret
    create api endpoint for updating secret by secret name
    
    secret name is passed through query params
    
    secret value is passed through JSON request body
    
    respond with 204 status and empty json body on successful update
    
    respond with 404 and error message on fail

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,7 @@ import '../utils/rds/loadEnv.js';
 import getAllSecrets from '../utils/rds/getAllSecrets.js';
 import getSecret from '../utils/rds/getSecret.js';
 import deleteSecret from '../utils/rds/deleteSecret.js';
+import updateSecret from '../utils/rds/updateSecret.js';
 
 const app = express();
 
@@ -42,6 +43,29 @@ app.delete('/secret', async (req, res) => {
         if (!secretDeleted) {
             throw new Error('Secret could not be deleted or was not found.');
         }
+        res.status(204).json({});
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+/*
+https://sequelize.org/docs/v7/querying/update/#updating-a-row-using-modelupdate
+According to this link, `Model#update` only updates the fields that you specify, so it's more appropriate to use `PATCH` than `PUT`.
+
+Secret name is sent through URL-encoded query params.
+Secret value is sent in JSON request body.
+*/
+app.patch('/secret', async (req, res) => {
+    try {
+        const secretName = req.query.name;
+        const secretValue = req.body.value;
+        const secretUpdated = !!(await updateSecret(secretName, secretValue));
+
+        if (!secretUpdated) {
+            throw new Error('Secret could not be updated or was not found.');
+        }
+        // returns no content
         res.status(204).json({});
     } catch (error) {
         res.status(404).json({ message: error.message });
