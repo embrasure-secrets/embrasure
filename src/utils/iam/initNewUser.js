8b9cf577776f77a34986091196d9d1dbbd2b851e
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-11-01 14:22:26 -0700

    fix(iam): :bug: only create user group if it doesn't exist
    added a check in initNewUser.js that first checks to see if a user group exists, and only creates a one if it does not. this way, the function doesn't error out when you run it more than once
    
    Co-Authored-By: ivanfslee <57885956+ivanfslee@users.noreply.github.com>

--- src/utils/iam/initNewUser.js
+++ src/utils/iam/initNewUser.js
@@ -1,5 +1,6 @@
 import createIAMUser from './createIAMUser.js';
 import createUserGroup from './createUserGroup.js';
+import doesUserGroupExist from './doesUserGroupExist.js';
 import addUserToUserGroup from './addUserToUserGroup.js';
 import generateAccessKeys from './generateAccessKey.js';
 import createNewDBPolicy from './createNewDBPolicy.js';
@@ -8,7 +9,9 @@ import attachPolicyToUser from './attachPolicyToUser.js';
 async function initNewUser(IAMUsername) {
     try {
         await createIAMUser(IAMUsername);
-        await createUserGroup('developer');
+        if (doesUserGroupExist(IAMUsername) === false) {
+            await createUserGroup('developer');
+        }
         await addUserToUserGroup('developer', IAMUsername);
         await generateAccessKeys(IAMUsername);
         console.log(`GIVE AWS ACCESS KEYS TO ${IAMUsername}`);
