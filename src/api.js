71513f21dfc2a1b6fe9c2286c67c2f35157f2b4b
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-11-01 19:44:15 -0700

    feat(api): :alembic: experiment with non-admin permissions
    experiment with using auth-token when a non-admin user runs embrasure getAllSecrets
    
    Co-Authored-By: Chase Sobey <116530802+ChaseSobey@users.noreply.github.com>
    Co-Authored-By: ivanfslee <57885956+ivanfslee@users.noreply.github.com>

--- src/api.js
+++ src/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 import './loadEnv.js';
+import generateDBAuthToken from './utils/iam/generateDBAuthToken.js';
+import isUserInGroup from './utils/aws-init/isUserInGroup.js';
+import getRegion from './utils/aws-init/getRegion.js';
+import getUsername from './utils/aws-init/getUsername.js';
 
 const ENDPOINT = process.env.API_ENDPOINT;
+const REGION = await getRegion();
+const IAM_USERNAME = await getUsername();
+
 // hard-coded for now
 const headers = {
     'db-username': process.env.DB_USER,
@@ -11,7 +18,19 @@ const headers = {
     'db-port': process.env.DB_PORT,
 };
 
+const NON_ADMIN_GROUP = 'embrasure-developer';
+
 export const getAllSecrets = async () => {
+    if (await isUserInGroup(IAM_USERNAME, NON_ADMIN_GROUP)) {
+        headers['db-auth-token'] = await generateDBAuthToken(
+            REGION,
+            headers['db-host'],
+            headers['db_port'],
+            IAM_USERNAME
+        );
+    }
+    console.log('value of headers[db-auth-token]: ', headers['db-auth-token']);
+    console.log('headers are: ', headers);
     const { data: secrets } = await axios.get(`${ENDPOINT}/secrets`, {
         headers,
     });
