80b440624bfb8f744c1121aab341e2fb5b2670e7
Chase Sobey <christopher.chase.sobey@gmail.com>
2023-11-09 16:52:07 -0800

    create deleteVpcSecurityGroup.js :sparkles:
    create function that takes the embrasure created security group ID as an argument and then deletes that security group

---
+++ src/aws/vpc/deleteVpcSecurityGroup.js
@@ -0,0 +1,21 @@
+import { EC2Client, DeleteSecurityGroupCommand } from '@aws-sdk/client-ec2';
+
+// Set up the EC2 client
+const client = new EC2Client(); // Replace 'your-region' with your AWS region
+
+async function deleteVpcSecurityGroup(securityGroupId) {
+    // Create the DeleteSecurityGroupCommand
+    const deleteSecurityGroupCommand = new DeleteSecurityGroupCommand({
+        GroupId: securityGroupId,
+    });
+
+    // Call the EC2 client to delete the security group
+    try {
+        const response = await client.send(deleteSecurityGroupCommand);
+        console.log('security group deleted. response: ', response);
+    } catch (error) {
+        console.error('Error deleting Security Group:', error);
+    }
+}
+
+export default deleteVpcSecurityGroup;
