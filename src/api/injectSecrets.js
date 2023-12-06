c5d006f065a4fbd32a59956901659b63cbd9488f
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-12-06 14:50:30 -0800

    feat(api): :sparkles: add secret value redaction
    add functionality to secret injection that will redact any secret values that are logged to the console by the child process

--- src/api/injectSecrets.js
+++ src/api/injectSecrets.js
@@ -1,21 +1,42 @@
 // import spawn method of node:child_process module
 import { spawn } from 'child_process';
+import { getAllSecrets } from './api.js';
 
 const NODE_PATH = process.execPath;
-function injectSecrets(FILE_NAME, env) {
+const REDACTION = '--SECRET REDACTED BY EMBRASURE--';
+
+async function injectSecrets(FILE_NAME, env) {
+    const secrets = await getAllSecrets();
+    const secretValues = secrets.map(({ value }) => value);
     const child = spawn(NODE_PATH, [FILE_NAME], {
         env,
     });
 
+    const handleStdout = (data) => {
+        let dataString = String(data);
+        secretValues.forEach((secretValue) => {
+            dataString = dataString.replace(secretValue, REDACTION);
+        });
+
+        process.stdout.write('Child Process Output:\n');
+        process.stdout.write(`${dataString}\n`);
+    };
+
+    const handleStderr = (data) => {
+        let dataString = String(data);
+        secretValues.forEach((secretValue) => {
+            dataString = dataString.replace(secretValue, REDACTION);
+        });
+
+        process.stderr.write('Child Process Error:\n');
+        process.stderr.write(`${dataString}\n`);
+    };
+
     // log child process output
-    child.stdout.on('data', (data) => {
-        console.log(`Child Process Output: ${data}`);
-    });
+    child.stdout.on('data', handleStdout);
 
     // log child process errors
-    child.stderr.on('data', (data) => {
-        console.error(`Child Process Error: ${data}`);
-    });
+    child.stderr.on('data', handleStderr);
 
     child.on('close', (code) => {
         console.log(`Child Process exited with code ${code}`);
